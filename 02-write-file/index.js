f5f46087c7a2315329a60d4666b44cdd659080fb
yurilabatsevich <yurilabatsevich@gmail.com>
2022-05-22 21:03:43 +0300

    fix:fix code

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,23 +1,24 @@
-const fs = require("fs");
-const path = require("path");
-const { stdin, stdout, exit } = require("node:process")
+/* eslint-disable indent */
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout, exit } = require('process');
 
-const output = fs.createWriteStream(path.join(__dirname, 'destination.txt'))
+const output = fs.createWriteStream(path.join(__dirname, 'destination.txt'));
 
-stdout.write('Привет! Введите строку или exit для завершения:\n')
+stdout.write('Привет! Введите строку или exit для завершения:\n');
 
 stdin.on('data', chunk => {
 
     if (chunk.toString().trim() === 'exit') {
-        exit()
+        exit();
     }
 
     output.write(chunk);
 
-})
+});
 
 
 process.on('exit', () => {
-    stdout.write('До встречи!')
-})
-process.on('SIGINT', () => exit())
+    stdout.write('До встречи!');
+});
+process.on('SIGINT', () => exit());
